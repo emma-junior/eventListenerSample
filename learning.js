07fa468654ed6ee9ae6abc262a2d7fac90df919a
unknown <emnzboy31@gmail.com>
2020-09-05 14:03:31 +0100

    Initial commit

---
+++ learning.js
@@ -0,0 +1,39 @@
+let button = document.querySelector('.button').addEventListener('click', buttonClick);
+
+function buttonClick(){
+    let first = document.querySelector('.first');
+    first.innerHTML = '<b>Engr. A.A. Rabiu</b><p>He is the president of COREN and the executive partner of AR-AR partnership, an indigenous consulting outfit with a professional practice embracing a full range of activities in civil engineering, water resources engineering and project management.</p>'  
+}
+
+let jeff = document.querySelector('.jeff').addEventListener('click', btnClick)
+
+function btnClick(){
+    let second = document.querySelector('.second');
+    second.innerHTML = '<b>Jeff Ewing</b><p>He is the managing director, chevron Nigeria limited</p>'
+}
+
+let herbert = document.querySelector('.herbert').addEventListener('click', butonClick)
+
+function butonClick(){
+    let third = document.querySelector('.third');
+    third.innerHTML = '<b>Herbert Onyewumbu Wigwe</b><p>He is the Managing Director/CEO/Co-Founder at Access Bank PLC.</p>'
+}
+
+let atinuke = document.querySelector('.atinuke').addEventListener('click', buttnClick)
+
+function buttnClick(){
+    let fourth = document.querySelector('.fourth')
+    fourth.innerHTML = '<b>Engr. Atinuke Wuraola Owolabi. M NSE. M NIEEE</b><p>She is the Managing director/CEO of Wurvicat solar foundation</p>'
+}
+
+let mike = document.querySelector('.mike').addEventListener('click', bttnClick)
+
+function bttnClick(){
+    let fifth = document.querySelector('.fifth')
+    fifth.innerHTML = '<b>Engr. Mike Sangster</b><p>He is the Managing Director/CEO of TOTAL E&P Nigeria Ltd </p>'
+}
+
+
+    
+   
+   
